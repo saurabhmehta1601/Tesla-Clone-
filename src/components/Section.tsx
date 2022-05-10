d6b4daf479725e8d5c2d846ca4abb39b7e58f31b
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-05-10 16:27:30 +0530

    update section component styles and make it dynamic with props

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import styled from "styled-components";
 import tw from "twin.macro";
 
-export const Section = () => {
+export const Section = ({
+  data: { title, description, darkButtonTitle, lightButtontitle, bgImage },
+}) => {
   return (
-    <Container>
+    <Container bgImage={bgImage}>
       <Text>
-        <Heading>Model S</Heading>
-        <SubHeading>Order Online for touchless delivery </SubHeading>
+        <Heading>{title}</Heading>
+        <SubHeading>{description}</SubHeading>
       </Text>
       <Wrapper>
         <ButtonGroup>
-          <DarkButton>Custom Order</DarkButton>
-          <LightButton>Existing Inventory</LightButton>
+          {darkButtonTitle && <DarkButton>{darkButtonTitle}</DarkButton>}
+          {lightButtontitle && <LightButton>{lightButtontitle}</LightButton>}
         </ButtonGroup>
         <DownArrow src="/images/down-arrow.svg" />
       </Wrapper>
@@ -20,11 +22,13 @@ export const Section = () => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  ${tw`relative -bottom-12 `}
+`;
 
 const Container = styled.div`
-  ${tw`h-screen flex flex-col justify-between text-center`}
-  background-image: url("/images/model-s.jpg");
+  ${tw`h-screen pt-12 pb-12 flex flex-col justify-between text-center `}
+  background-image: ${(props) => `url(/images/${props.bgImage})`};
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -32,18 +36,18 @@ const Container = styled.div`
 
 const Text = styled.div``;
 const Heading = styled.h1`
-  ${tw`text-6xl font-bold text-gray-800 m-0 p-4`}
+  ${tw`text-6xl font-bold text-gray-800 m-0 `}
 `;
 const SubHeading = styled.h3`
-  ${tw`text-2xl text-gray-600 mt-2`}
+  ${tw`text-2xl text-gray-600  mt-2`}
 `;
 const ButtonGroup = styled.div`
-  ${tw` flex flex-col row-gap-2  mx-auto `}
-  width:fit-content;
+  ${tw` flex justify-around row-gap-4 items-center mx-auto pb-12 xl:flex-row flex-col`}
 `;
 
 const Button = styled.button`
-  ${tw`bg-white text-lg text-black font-bold py-2 px-36 rounded-full border-none uppercase cursor-pointer w-full`}
+  ${tw`opacity-95 tracking-widest  bg-white text-sm text-black font-bold py-4 px-36 rounded-full border-none uppercase cursor-pointer w-full max-w-xl`}
+  word-spacing: 0.3rem;
 `;
 const DarkButton = styled(Button)`
   ${tw`bg-gray-800 text-white `}
@@ -51,4 +55,5 @@ const DarkButton = styled(Button)`
 const LightButton = styled(Button)``;
 const DownArrow = styled.img`
   ${tw`w-12 h-12`}
+  animation: animateDownArrow 1.5s  infinite;
 `;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,69 @@ import { Section } from "../components/Section";
 export default function Home() {
   return (
     <Container>
-      <Section />
+      <Section
+        data={{
+          title: "Model S",
+          description: "Order Online for touchless delivery",
+          darkButtonTitle: "Custom Order",
+          lightButtontitle: "Existing Inventory",
+          bgImage: "model-s.jpg",
+        }}
+      />
+      <Section
+        data={{
+          title: "Model Y",
+          description: "Order Online for touchless delivery",
+          darkButtonTitle: "Custom Order",
+          lightButtontitle: "Existing Inventory",
+          bgImage: "model-y.jpg",
+        }}
+      />
+      <Section
+        data={{
+          title: "Model 3",
+          description: "Order Online for touchless delivery",
+          darkButtonTitle: "Custom Order",
+          lightButtontitle: "Existing Inventory",
+          bgImage: "model-3.jpg",
+        }}
+      />
+      <Section
+        data={{
+          title: "Model X",
+          description: "Order Online for touchless delivery",
+          darkButtonTitle: "Custom Order",
+          lightButtontitle: "Existing Inventory",
+          bgImage: "model-x.jpg",
+        }}
+      />
+      <Section
+        data={{
+          title: "Lowest Cost Solar Panels",
+          description: "Money back guarantee",
+          darkButtonTitle: "Order Now",
+          lightButtontitle: "Learn More",
+          bgImage: "solar-panel.jpg",
+        }}
+      />
+      <Section
+        data={{
+          title: "Solar for new Roofs",
+          description:
+            "Solar Roofs Costs Less Than a New Roof Plus Solar Panels",
+          darkButtonTitle: "Order Now",
+          lightButtontitle: "Learn More",
+          bgImage: "solar-roof.jpg",
+        }}
+      />
+      <Section
+        data={{
+          title: "Accessories",
+          description: "Our Most Popular Accessories",
+          darkButtonTitle: "Order Now",
+          bgImage: "accessories.jpg",
+        }}
+      />
     </Container>
   );
 }
