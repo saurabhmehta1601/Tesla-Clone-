c5908eebb859ca0fcbf955c51430443e1da69266
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-05-10 11:46:41 +0530

    move styles ,components ,pages to src folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 import tw from "twin.macro";
+import { Header } from "../components/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <Heading>THis is heading</Heading>
     </div>
   );
