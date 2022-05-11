2ab6a287dc33517cc3ec9f7cb00cee5771f8d150
Saurabh Mehta <saurabhmehta1601@gmail.com>
2022-05-11 12:41:13 +0530

    add Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,29 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <Logo>
+        <img src="/images/logo.svg" />
+      </Logo>
+      <Menu>
+        <MenuItem>Model S</MenuItem>
+        <MenuItem>Model 3</MenuItem>
+        <MenuItem>Model X</MenuItem>
+        <MenuItem>Model Y</MenuItem>
+      </Menu>
+    </Container>
+  );
 };
+
+const Container = styled.header`
+  ${tw`flex py-6 px-8 uppercase bg-blue-600 `}
+  position: sticky;
+  position: -webkit-sticky;
+`;
+const Logo = styled.a`
+  ${tw`flex-1 `}
+`;
+const Menu = styled.div``;
+const MenuItem = styled.a``;
